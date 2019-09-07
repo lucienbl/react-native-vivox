7aaeb70321b9c0c0f18b939661ef04d374531d74
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2019-09-07 14:24:54 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,4 +2,30 @@ import { NativeModules } from 'react-native';
 
 const { Vivox } = NativeModules;
 
-export default Vivox;
+export const connect = async (serverUrl: string, issuer: string, realm: string) => {
+  await Vivox.connect(serverUrl, issuer, realm);
+};
+
+export const login = (userId: string, token: string) => {
+  setTimeout(async () => {
+    await Vivox.setLoginCredentials(userId, token);
+  }, 100);
+};
+
+export const joinChannel = (channelId: string, token: string) => {
+  this.setTimeout(async () => {
+    await Vivox.joinMatch(channelId, token);
+  }, 100);
+};
+
+export const leaveChannel = async () => {
+  await Vivox.leaveMatch();
+};
+
+export const disconnect = async () => {
+  await Vivox.disconnect();
+};
+
+export const getStateName = async (): string => {
+  return Vivox.getStateName();
+};
