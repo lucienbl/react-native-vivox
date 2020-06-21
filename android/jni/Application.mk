e5a8c39c03d6a3d5208e4a20c7101a6abdb780b7
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2020-06-21 23:18:16 +0200

    chore: refactored make files

--- android/jni/Application.mk
+++ android/jni/Application.mk
@@ -1,19 +1,22 @@
-APP_PLATFORM := android-14
-APP_PIE := true
 APP_STL := c++_static
-APP_ABI := armeabi-v7a x86 arm64-v8a
+# APP_STL := c++_shared
+
+APP_ABI := armeabi-v7a x86 x86_64 arm64-v8a
 
 APP_CFLAGS += -ffast-math -D__ANDROID__
 # APP_CFLAGS += -Wall -Wextra
 # APP_CFLAGS += -Werror
-APP_CFLAGS += -DHAVE_ASCTIME_R=1 -DHAVE_CTIME_R=1 -DHAVE_GMTIME_R=1 -DHAVE_LOCALTIME_R=1
+APP_CFLAGS += -DHAVE_ASCTIME_R=1 -DHAVE_CTIME_R=1 -DHAVE_GMTIME_R=1 -DHAVE_LOCALTIME_R=1 -D__STDC_FORMAT_MACROS=1
 
 ifeq (1,$(NDK_DEBUG))
+APP_VARIANT := Debug
 APP_OPTIM := debug
 APP_CFLAGS += -O0 -g -DDEBUG -D_DEBUG
-# APP_CFLAGS += -ftrapv # trap in of integer overflow
 APP_CFLAGS += -DUSE_PARANOID_ALLOCATOR
+# APP_CFLAGS += -ftrapv # trap in of integer overflow
 else
+APP_VARIANT := Release
 APP_OPTIM := release
 APP_CFLAGS += -O3 -DNDEBUG
 endif
+APP_CPPFLAGS += -std=gnu++11
