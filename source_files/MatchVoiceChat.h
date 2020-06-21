f64d3c351d0b953cc5f00fc394ff9d8793ce33be
Lucien Blunk-Lallet <lucien.blunk.lallet@gmail.com>
2020-06-21 23:18:00 +0200

    chore: allow building android

--- source_files/MatchVoiceChat.h
+++ source_files/MatchVoiceChat.h
@@ -1,5 +1,5 @@
-#include <TargetConditionals.h>
-#if !TARGET_OS_SIMULATOR
+#include "Platform.h"
+#if !IOS_SIMULATOR
 /* Copyright (c) 2016-2018 by Mercer Road Corp
  *
  * Permission to use, copy, modify or distribute this software in binary or source form
